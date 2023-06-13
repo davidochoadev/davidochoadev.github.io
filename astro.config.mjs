e87dac807038f4a78391f4a58d1977101e074c1e
davideochoaa <davideochoaa@gmail.com>
2023-06-14 00:57:33 +0200

    🛠 Fix: Fixing astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidochoadev.github.io',
-  base: '/davidochoadev',
   integrations: [tailwind(), react()]
 });
