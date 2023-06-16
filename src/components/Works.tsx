be894320b193ab139785136e75adb7ecf0434fca
davideochoaa <davideochoaa@gmail.com>
2023-06-17 01:27:02 +0200

    ✅ Feat: Created dotlottie import

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,44 +1,7 @@
 import { useState } from 'react';
 import requestedPosts from './projects.json';
+import '@dotlottie/player-component';
 
-interface PaginationProps {
-	currentPage: number;
-	nPages: number;
-	setCurrentPage: (page: number) => void;
-}
-
-function Pagination({ currentPage, nPages, setCurrentPage }: PaginationProps) {
-	const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
-	const nextPage = () => {
-		if (currentPage !== nPages) setCurrentPage(currentPage + 1);
-	};
-	const prevPage = () => {
-		if (currentPage !== 1) setCurrentPage(currentPage - 1);
-	};
-	return (
-		<nav className="flex w-full justify-center">
-			<ul className="flex flex-row gap-2 self-center">
-				<li className="bg-slate-400 p-2">
-					<a className="page-link" onClick={prevPage} href="#">
-						Previous
-					</a>
-				</li>
-				{pageNumbers.map((pgNumber) => (
-					<li key={pgNumber} className={`page-item ${currentPage == pgNumber ? 'active' : ''} `}>
-						<a onClick={() => setCurrentPage(pgNumber)} className="page-link" href="#">
-							{pgNumber}
-						</a>
-					</li>
-				))}
-				<li className="bg-slate-400 p-2">
-					<a className="page-link" onClick={nextPage} href="#">
-						Next
-					</a>
-				</li>
-			</ul>
-		</nav>
-	);
-}
 export function Works() {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [recordsPerPage] = useState(4);
@@ -65,32 +28,43 @@ export function Works() {
 
 	return (
 		<div className="tranistion-all flex min-h-[300px] flex-row flex-wrap items-center gap-5 rounded-xl bg-gradient-to-tl from-slate-950 via-slate-900 to-[#05604b] p-4 shadow-[inset_4px_4px_10px_rgba(255,255,255,.2)] backdrop-blur-xl duration-300 ease-in-out hover:scale-105 hover:rounded-br-3xl hover:rounded-tl-3xl lg:w-2/3 lg:max-w-[830px] lg:rounded-3xl lg:rounded-br lg:rounded-tl lg:p-14">
-         <div>
-            {currentPage !== 1 ? ( 
-               <button onClick={prevPage} className='p-2 bg-slate-300 '>
-                  Previous
-               </button>) : (
-               <button className='p-2 bg-slate-300 disabled:opacity-50' disabled>
-                  Previous
-               </button>
-               )
-            }
-         </div>
-         <div className='flex-grow lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-4'>
-            {currentRecords.map((item, index) => (
-               <div key={index} className='backdrop-blur-md bg-slate-700/50 p-4 rounded-xl'>
-                  <img src={`https://raw.githubusercontent.com/davidochoadev${item.imageUrl}`} alt="" height={'200px'} width={'200px'}/>
-                  <p className='text-white'>{item.projectName}</p>
-               </div>
-            ))}
-         </div>
-         <div>
-         {currentPage !== nPages &&
-            <button onClick={nextPage} className='p-2 bg-slate-300'>
-               Next
-            </button>
-         }
-         </div>
+      <dotlottie-player
+        src="./img/animation.lottie"
+        autoplay
+        loop
+        style={{ height: '100%', width: '100%' }}
+      />
+			<div>
+				{currentPage !== 1 ? (
+					<button onClick={prevPage} className="bg-slate-300 p-2 ">
+						Previous
+					</button>
+				) : (
+					<button className="bg-slate-300 p-2 disabled:opacity-50" disabled>
+						Previous
+					</button>
+				)}
+			</div>
+			<div className="flex-grow lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-4">
+				{currentRecords.map((item, index) => (
+					<div key={index} className="rounded-xl bg-slate-700/50 p-4 backdrop-blur-md">
+						<img
+							src={`https://raw.githubusercontent.com/davidochoadev${item.imageUrl}`}
+							alt=""
+							height={'200px'}
+							width={'200px'}
+						/>
+						<p className="text-white">{item.projectName}</p>
+					</div>
+				))}
+			</div>
+			<div>
+				{currentPage !== nPages && (
+					<button onClick={nextPage} className="bg-slate-300 p-2">
+						Next
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }
