701721d193baaae32a02ca19930699b689213eaa
davideochoaa <davideochoaa@gmail.com>
2023-06-17 02:46:18 +0200

    🛠 Fix: Fix of pagination styling

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import requestedPosts from './projects.json';
-import '@dotlottie/player-component';
 
 export function Works() {
 	const [currentPage, setCurrentPage] = useState(1);
@@ -12,13 +11,6 @@ export function Works() {
 	const currentRecords = projects.slice(indexOfFirstRecord, indexOfLastRecord); // elementi correnti
 	const nPages = Math.ceil(projects.length / recordsPerPage);
 
-	const getRangeOfItemsToShow = (pageNumber: number) => {
-		const startIndex = (pageNumber - 1) * recordsPerPage;
-		const endIndex = startIndex + recordsPerPage;
-
-		return projects.slice(startIndex, endIndex);
-	};
-
 	const nextPage = () => {
 		if (currentPage !== nPages) setCurrentPage(currentPage + 1);
 	};
@@ -27,42 +19,72 @@ export function Works() {
 	};
 
 	return (
-		<div className="tranistion-all flex min-h-[300px] flex-row flex-wrap items-center gap-5 rounded-xl bg-gradient-to-tl from-slate-950 via-slate-900 to-[#05604b] p-4 shadow-[inset_4px_4px_10px_rgba(255,255,255,.2)] backdrop-blur-xl duration-300 ease-in-out hover:scale-105 hover:rounded-br-3xl hover:rounded-tl-3xl lg:w-full lg:rounded-3xl lg:rounded-br lg:rounded-tl lg:p-14">
-		<div className="absolute w-full h-full">
-			<dotlottie-player
-			src="./img/animation.lottie"
-			autoplay
-			loop
-			/>
-		</div>
-			<div>
+		<div className="tranistion-all flex min-h-[300px] flex-row items-center gap-5 rounded-xl bg-gradient-to-tl from-slate-950 via-slate-900 to-[#05604b] p-4 shadow-[inset_4px_4px_10px_rgba(255,255,255,.2)] backdrop-blur-xl duration-300 ease-in-out hover:scale-105 hover:rounded-br-3xl hover:rounded-tl-3xl lg:w-full lg:rounded-3xl lg:rounded-br lg:rounded-tl lg:p-14">
+			<div className="w-1/12">
 				{currentPage !== 1 ? (
-					<button onClick={prevPage} className="bg-slate-300 p-2 ">
-						Previous
+					<button onClick={prevPage} className="p-4 rounded-full bg-gradient-to-tl from-slate-900 to-[#05604b] shadow-md drop-shadow-md transition duration-300 hover:scale-125">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="white"
+							className="bi bi-chevron-left"
+							viewBox="0 0 16 16"
+						>
+							<path
+								fillRule="evenodd"
+								d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+							/>
+						</svg>
 					</button>
 				) : (
-					<button className="bg-slate-300 p-2 disabled:opacity-50" disabled>
-						Previous
+					<button className="bg-gradient-to-tl from-slate-950/50 to-[#05604b]/50 p-4 disabled:opacity-40 rounded-full opacity-10 shadow-md drop-shadow-md" disabled>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="white"
+							className="bi bi-chevron-left"
+							viewBox="0 0 16 16"
+						>
+							<path
+								fillRule="evenodd"
+								d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+							/>
+						</svg>
 					</button>
 				)}
 			</div>
-			<div className="flex-grow lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-4">
+			<div className="grid h-full w-full grid-cols-2 grid-rows-2 place-items-center gap-8">
 				{currentRecords.map((item, index) => (
-					<div key={index} className="rounded-xl bg-slate-700/50 p-4 backdrop-blur-md">
+					<div key={index} className="w-[200px] md:w-[250px] lg:w-[400px] md:h-autorounded-xl bg-slate-700/50 p-8 backdrop-blur-md flex justify-center flex-col items-center">
 						<img
 							src={`https://raw.githubusercontent.com/davidochoadev${item.imageUrl}`}
 							alt=""
-							height={'200px'}
-							width={'200px'}
+							height={'100%'}
+							width={'100%'}
+							className='md:w-[200px] md:h-auto'
 						/>
 						<p className="text-white">{item.projectName}</p>
 					</div>
 				))}
 			</div>
-			<div>
+			<div className="w-1/12">
 				{currentPage !== nPages && (
-					<button onClick={nextPage} className="bg-slate-300 p-2">
-						Next
+					<button onClick={nextPage} className="p-4 rounded-full bg-gradient-to-tl from-slate-950 to-[#05604b] shadow-md drop-shadow-md transition duration-300 hover:scale-125">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="white"
+							className="bi bi-chevron-right"
+							viewBox="0 0 16 16"
+						>
+							<path
+								fillRule="evenodd"
+								d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+							/>
+						</svg>
 					</button>
 				)}
 			</div>
