a18ebbfa50cbe2e8c6d59333037f30efb4ab2283
davidochoadev <davidochoa.dev@gmail.com>
2024-01-02 05:32:08 +0100

    ✅ Feat: Created StackButton component and text edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidochoadev.github.io',
-  integrations: [tailwind(), react(), image()],
-  experimental: {
-    assets: true
-   }
+  integrations: [tailwind(), react()]
 });
