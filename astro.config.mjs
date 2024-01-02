8bdc8a2aee0660340554f4fb957896192f7eb687
davidochoadev <davidochoa.dev@gmail.com>
2024-01-02 06:18:35 +0100

    ✅ Feat: Added partytown/gtag.js and some integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidochoadev.github.io',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sentry(), spotlightjs(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		})
+  ]
 });
